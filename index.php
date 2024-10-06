ecf26349642a1d83a2d2ca4b58c96184df854235
akinori <aki10.working@gmail.com>
2024-10-07 01:01:13 +0900

    first commit

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php get_header(); ?>
 
 <div class="container mx-auto p-5">
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5">
+    <div class="grid grid-cols-2 md:grid-cols-5 gap-5">
         <?php
         // メインループを使用して投稿を取得
         if ( have_posts() ) :
             while ( have_posts() ) : the_post(); ?>
                 <div class="gallery-item text-center">
                     <a href="<?php the_permalink(); ?>">
-                          <div class="aspect-w-1 aspect-h-1">
-                            <img src="<?= esc_url(get_field('image_0')); ?>" alt="<?= the_title() ?>">
+                          <div class="aspect-square">
+                            <img src="<?= esc_url(get_field('image_0')); ?>" alt="<?= the_title() ?>" class="object-cover w-full h-full">
                           </div>
                         <h2 class="mt-2 text-lg font-bold text-gray-800"><?php the_title(); ?></h2>
                     </a>
